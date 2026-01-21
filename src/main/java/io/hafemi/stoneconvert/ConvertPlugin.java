1859730f3bb91e0228922efb0c762f36941c304f
hafemi <118757157+hafemi@users.noreply.github.com>
2026-01-21 08:17:53 +0100

    Initial commit

---
+++ src/main/java/io/hafemi/stoneconvert/ConvertPlugin.java
@@ -0,0 +1,16 @@
+package io.hafemi.stoneconvert;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class ConvertPlugin extends JavaPlugin {
+    public ConvertPlugin(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+}
